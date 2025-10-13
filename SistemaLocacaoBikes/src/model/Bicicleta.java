9ef29eb395bb297a8365dd714175c3057294868f
Gabriel Fernando <gbrlfrnndsz@gmail.com>
2025-10-13 08:46:54 -0300

    Aula 3 feita

---
+++ SistemaLocacaoBikes/src/model/Bicicleta.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Bicicleta {
+    private int id;
+    private String codigo;
+    private String status;
+    
+    // Getters e Setters
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    
+    public String getStatus(){
+        return status;
+    }
+    
+    public void setStatus(String status){
+        this.status = status;
+    }
+}
